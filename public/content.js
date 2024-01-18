ef2c2b492f68bcf3c3aa94a84bfd597815e9b706
raman7070 <ramansaxena@affidabilesolutions.com>
2024-01-18 14:37:48 +0530

    first commit

---
+++ public/content.js
@@ -0,0 +1,31 @@
+let lastScrollY = window.scrollY;
+
+const getUsername = () => {
+    const firstTweet = document.querySelector('[data-testid="tweet"]');
+    if (firstTweet) {
+        const usernameElement = firstTweet.querySelector('[data-testid="User-Name"]');
+        if (usernameElement) {
+            const username = usernameElement.textContent.trim();
+            console.log(`Username: ${username}`);
+        }
+    }
+};
+
+const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+    const scrollDirection = currentScrollY > lastScrollY ? 'down' : 'up';
+
+    if (scrollDirection === 'up' && lastScrollY !== 0) {
+        console.log('User has scrolled up');
+    } else if (scrollDirection === 'down') {
+        console.log('User has scrolled down');
+    }
+    getUsername();
+
+    lastScrollY = currentScrollY;
+};
+
+window.addEventListener('scroll', handleScroll);
+
+
+handleScroll();
